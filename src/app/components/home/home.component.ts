5f00e8471a87fda70117eb352d14f4eb39221b4e
Ambre Saint-Marc <saintmarc.ambre@gmail.com>
2025-02-05 00:44:17 +0100

    Added the filter by name function

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { Product, ProductsService } from '../../services/products.service';
 import { ProductItemComponent } from '../product-item/product-item.component';
 import { SortByPricePipe } from '../../pipes/sortByprice/sort-by-price.pipe';
@@ -15,8 +15,29 @@ import { SortByPricePipe } from '../../pipes/sortByprice/sort-by-price.pipe';
 
 export class HomeComponent implements OnInit{
   public products : Product[] = []
+  productsList: Product[] = [];
+  productsService = inject(ProductsService);
+  filteredProductsList: Product[] = [];
 
-  constructor(private service: ProductsService) { }
+
+  constructor(private service: ProductsService) { 
+    this.productsList = this.productsService.getProducts();
+    this.filteredProductsList = this.productsList;
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredProductsList = this.productsList;
+      return;
+    }
+
+    this.filteredProductsList = this.productsList.filter((products) =>
+      products?.name.toLowerCase().includes(text.toLowerCase()),
+    );
+  }
+
+
+
 
   ngOnInit(): void {
     this.products = this.service.getProducts();
